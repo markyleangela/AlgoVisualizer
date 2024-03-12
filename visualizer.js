17d2cabeb3300e82e23f200a42166a106050663b
markyleangela <markyleangela@gmail.com>
2024-03-13 04:09:30 +0800

    Aligned canvas and objects; Added buttons

--- visualizer.js
+++ visualizer.js
@@ -1,19 +1,39 @@
 
-var arr = [1,2,4,2,5,1,2]
+ //maximum of 25
+ var arr = new Array(25);
+ 
+ for(let i = 0; i < arr.length; i++){
+        arr[i] = (Math.floor(Math.random() * 10) + 1);
+  }
+
+function randomize(){
+  for(let i = 0; i < arr.length; i++){
+    arr[i] = (Math.floor(Math.random() * 10) + 1);
+  }
+}
+
 function draw(canvasID) {
     const canvas = document.getElementById(canvasID);
     if (canvas.getContext) {
       const ctx = canvas.getContext("2d");
-        var x = 20;
+      var x = 20;
+      
       for(var i = 0; i < arr.length; i++){
-        ctx.fillRect(x,20,50,arr[i] * 100)
-        x+=80;
+        ctx.fillRect(x,10,5,arr[i] * 10)
+        x+=10;
       }
     }
-  }
+}
+
 
 
 draw("canvas1");
+
+document.querySelector(".random-button").addEventListener("click" , function(){
+  location.reload();
+});
+
+
 
 //this is how to add canvas in html 
 // var canv = document.createElement("canvas");
@@ -22,3 +42,13 @@ draw("canvas1");
 
 
 // draw("canvas2",20,20, 100);
+
+
+//var randomButton = document.getElementsByClassName("random-button")[0];
+//  randomButton.addEventListener("click", function(){
+//    arr = new Array(25);
+//    for(let i = 0; i < arr.length; i++){
+//      arr[i] = (Math.floor(Math.random() * 10) + 1);
+//    }
+//    draw("canvas1");
+//  })
