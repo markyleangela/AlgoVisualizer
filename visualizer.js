508986475803bf88c47e1b6cf39178f888a52979
markyleangela <markyleangela@gmail.com>
2024-03-20 04:42:17 +0800

    Started animation

--- visualizer.js
+++ visualizer.js
@@ -1,54 +1,73 @@
 
  //maximum of 25
- var arr = new Array(25);
- 
- for(let i = 0; i < arr.length; i++){
-        arr[i] = (Math.floor(Math.random() * 10) + 1);
-  }
+ var arr = new Array(25); // store values of each bar
+ var allBar = []; // store all bars
 
 function randomize(){
   for(let i = 0; i < arr.length; i++){
     arr[i] = (Math.floor(Math.random() * 10) + 1);
   }
 }
+randomize();
 
-function draw(canvasID) {
-    const canvas = document.getElementById(canvasID);
-    if (canvas.getContext) {
-      const ctx = canvas.getContext("2d");
-      var x = 20;
-      
-      for(var i = 0; i < arr.length; i++){
-        ctx.fillRect(x,10,5,arr[i] * 10)
-        x+=10;
-      }
-    }
+const canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+
+class Bar {
+  constructor(xpos, ypos =10, w, h, speed){
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.h = h;
+    this.speed = speed;
+    this.dx = 1 * this.speed;
+    this.dy = 1 * this.speed;
+  }
+
+  draw(ctx){
+    ctx.fillRect(this.xpos, this.ypos, 5, this.h);
+  }
+
+  update(){
+    ctx.clearRect(0,0, 500, 850);
+    this.draw(ctx);
+
+    this.xpos += this.dx;
+    // this.ypos += this.dy;
+  }
 }
 
 
+// function drawAll(){
+//   var x = 20;
+//   for(let i = 0; i < arr.length; i ++){
+//     var bar = new Bar(x, 10, 5, arr[i] * 10);
+//     allBar.push(bar);
+//     bar.draw(ctx);
+//     x+=10;
+    
+//   }
+// }
 
-draw("canvas1");
+// drawAll();
+
+
+var bar = new Bar(10, 10, 5, 50, 1);
+var bar2 = new Bar(20, 100, 5, 50, 1);
+bar.draw(ctx);
+bar2.draw(ctx);
+
+let updateRect = function(){
+  requestAnimationFrame(updateRect);
+  bar.update();
+}
+
+updateRect();
+
+
+
+
+
 
 document.querySelector(".random-button").addEventListener("click" , function(){
   location.reload();
 });
-
-
-
-//this is how to add canvas in html 
-// var canv = document.createElement("canvas");
-// canv.setAttribute("id", "canvas2");;
-// document.body.appendChild(canv);
-
-
-// draw("canvas2",20,20, 100);
-
-
-//var randomButton = document.getElementsByClassName("random-button")[0];
-//  randomButton.addEventListener("click", function(){
-//    arr = new Array(25);
-//    for(let i = 0; i < arr.length; i++){
-//      arr[i] = (Math.floor(Math.random() * 10) + 1);
-//    }
-//    draw("canvas1");
-//  })
